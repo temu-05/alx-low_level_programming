5f60897111581980888d53229bba4c4b3cab332e
temu-05 <semeteme2@gmail.com>
2022-03-15 19:41:34 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,14 @@
-
+lphabet - prints to string
+ * Description: Prints alphabet in lowercase
+ */
+#include "main.h"
+ void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+return;
+}
