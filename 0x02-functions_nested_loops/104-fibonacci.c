6d93f25d96bf67c49a8ae42264ac9e0b8586d03e
temu-05 <semeteme2@gmail.com>
2022-03-16 10:19:56 +0000

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * numLength - returns the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+int length = 0;
+if (!num)
+return (1);
+while (num)
+{
+num = num / 10;
+length += 1;
+}
