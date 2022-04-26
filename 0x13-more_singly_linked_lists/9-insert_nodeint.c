ff0032a68ecf5262434eb389727152dc28706f17
temu-05 <99035848+temu-05@users.noreply.github.com>
2022-04-26 17:54:01 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,43 +1,48 @@
 #include "lists.h"
+
 /**
- * insert_nodeint_at_index - Entry Point
- * @head: head
- * @idx: index
- * @n: data
- * Return: 0
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: head of linked list
+ * @idx: index to insert the node
+ * @n: new node data
+ * Return: new node
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
-
+	listint_t *current;
 	listint_t *new;
-	listint_t *temp;
 
-	if (*head == NULL)
-		return (NULL);
+	if (head == NULL)
+		return (0);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
 
-	temp = *head;
+	new->next = NULL;
+	new->n = n;
 
-	while (temp != NULL)
+	if (idx == 0)
 	{
-		if (count == idx - 1)
-		{
-			new = malloc(sizeof(listint_t));
-			if (new == NULL)
-			{
-				return (NULL);
-			}
-			else
-			{
-				new->n = n;
-				new->next = temp->next;
-				temp->next = new;
-				return (new);
-			}
-		}
-		if (temp->next != NULL)
-			temp = temp->next;
-		count++;
+		new->next = *head;
+		(*head) = new;
+		return (new);
 	}
-	return (NULL);
+
+	current = *head;
+
+	while (idx != 1)
+	{
+		current = current->next;
+		--idx;
+		if (current == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+	new->next = current->next;
+	current->next = new;
+
+	return (new);
 }
