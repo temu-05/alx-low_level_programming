80659e4effe10078ba2baf114b96849beb224044
temu-05 <99035848+temu-05@users.noreply.github.com>
2022-04-26 17:50:50 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 /**
- * free_listint - Entry Point
- * @head: head
- * Return: 0
+ *free_listint - The function that free a memory location
+ *@head: the head of the linked list
  */
+
 void free_listint(listint_t *head)
 {
+	listint_t *tmp;
+
 	if (head == NULL)
+
 		return;
 
 	while (head)
+
 	{
+		tmp = head->next;
 		free(head);
-		head = head->next;
+		head = tmp;
 	}
+	free(head);
 }
